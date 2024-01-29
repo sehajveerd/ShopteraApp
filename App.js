51ba6fb003dc877e9ef9443dd76ee69ca646a44d
misry <dmisry.7.21.04@gmail.com>
2024-01-28 22:38:53 -0500

    finished layout for questionnaire 1-4, font not fixed

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import SplashScreen from "./pages/SplashScreen";
 import SignUpProcess from "./pages/SignUpProcess";
-import Questionnaire from "./pages/Questionnaire";
+import Questionnaire1 from "./pages/Questionnaire1";
+import Questionnaire2 from "./pages/Questionnaire2";
+import Questionnaire3 from "./pages/Questionnaire3";
+import Questionnaire4 from "./pages/Questionnaire4";
 import WelcomeScreen from "./pages/WelcomeScreen";
 
 export default function App() {
