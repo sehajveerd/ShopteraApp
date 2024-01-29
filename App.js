11d5da3bb304bf3f6f19f1eede21ea9a9ee24e47
sehajveerd <128254105+sehajveerd@users.noreply.github.com>
2024-01-28 20:29:31 -0500

    recap pg and name pg

--- App.js
+++ App.js
@@ -4,19 +4,13 @@ import SplashScreen from "./pages/SplashScreen";
 import SignUpProcess from "./pages/SignUpProcess";
 import Questionnaire from "./pages/Questionnaire";
 import WelcomeScreen from "./pages/WelcomeScreen";
-<<<<<<< HEAD
+import LoginScreen from "./pages/LoginScreen";
+import NotiScreen from "./pages/NotiScreen";
 import RecapOfPreferences from "./pages/RecapOfPreferences";
 import LegalName from "./pages/LegalName";
 
 export default function App() {
-  return <LegalName/>;
-=======
-import LoginScreen from "./pages/LoginScreen";
-import NotiScreen from "./pages/NotiScreen";
-
-export default function App() {
-  return <WelcomeScreen />;
->>>>>>> 6be60b13122bb9877f9aac7e196728909703edd4
+  return <RecapOfPreferences/>;
 }
 
 
